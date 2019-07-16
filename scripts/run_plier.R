ed79e2914f7e54c64b67ee9b7a1d64e5eeda05db
Jake Crawford <jjc2718@gmail.com>
2019-07-16 17:04:16 -0400

    script to run PLIER, now need to integrate into pipeline

--- scripts/run_plier.R
+++ scripts/run_plier.R
@@ -23,20 +23,12 @@ process_data <- function(data) {
     data_rowsums <- rowSums(data)
     inds <- which(data_rowsums != 0)
     if (length(inds) < data_dim[1]) {
-        warning(paste0("Subsetting data matrix to remove rows without measurements: ", data_dim[1], " -> ", length(inds)))
+        warning(paste0("Subsetting data matrix to remove rows without measurements: ",
+                       data_dim[1], " -> ", length(inds)))
         data <- data[inds, ]
         data_dim <- dim(data)
     }
     data
-}
-
-resolve_gene_names <- function(canonicalPathways, entrez_mapping) {
-    mapping = read.csv(entrez_mapping, header=T, sep='\t')
-    gene_symbols = rownames(canonicalPathways)
-    gene_eids = sapply(gene_symbols,
-                       function(x) mapping[mapping$symbol == x, 1])
-    pathways <- setattr(copy(canonicalPathways, 'row.names', gene_eids)
-    pathways
 }
 
 run_plier <- function(args) {
@@ -44,25 +36,29 @@ run_plier <- function(args) {
     if (args$verbose) {
         cat('Loading and preprocessing data...\n')
     }
-    data <- read.csv(args$data, header=T, sep='\t', row.names=1)
+    data <- read.csv(args$data, sep='\t', header=T, row.names=1, check.names=F)
     processed_data <- process_data(data)
+    processed_data <- as.matrix(processed_data)
+    pathways <- read.csv(args$pathway_file, sep='\t', header=T, row.names=1)
+    pathways <- as.matrix(pathways)
 
     if (args$verbose) {
         cat('Running PLIER...\n')
     }
 
-    # MSigDB canonical pathways data (see PLIER paper)
-    data(canonicalPathways)
-    # convert gene symbols to Entrez gene names in pathway data
-    pathways = resolve_gene_names(canonicalPathways, args$entrez_mapping)
-    plierResult <- PLIER(data, pathways)
+    # MSigDB canonical pathways data (see PLIER paper), with gene
+    # symbols mapped to Entrez IDs (see preprocessing notebook)
+    plierResult <- PLIER(processed_data, pathways, k=args$k, seed=args$seed)
+    write.table(plierResult$Z, file=args$output_file, quote=F, sep='\t')
 }
 
 main <- function() {
     parser <- ArgumentParser(description='Script to run PLIER on TCGA data')
     parser$add_argument('--data', required=T)
     parser$add_argument('--k', type='integer', required=T)
-    parser$add_argument('--entrez_mapping', default='data/entrez_mapping.tsv')
+    parser$add_argument('--output_file', required=T)
+    parser$add_argument('--pathway_file',
+                        default='data/pathway_data/canonical_mapped.tsv')
     parser$add_argument('--seed', type='integer', required=T)
     parser$add_argument('--verbose', action='store_true')
     args <- parser$parse_args()
