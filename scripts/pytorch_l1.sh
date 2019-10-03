11c90f0794cd1890aa11802228af67bcd3f9dc5c
Jake Crawford <jjc2718@gmail.com>
2019-10-03 09:14:25 -0400

    small cleanup things

--- scripts/pytorch_l1.sh
+++ scripts/pytorch_l1.sh
@@ -1,5 +1,4 @@
-# l1_pens=( "0" "0.01" "0.05" "0.1" "0.2" "0.5" )
-l1_pens=( "1" "5" "10" )
+l1_pens=( "0" "0.01" "0.05" "0.1" "0.2" "0.5" )
 
 for l1 in ${l1_pens[@]}; do
     cmd="python pytorch_vs_sklearn.py --gene TP53 --v --gpu --seed 1 --b 50 --learning_rate 0.00005 --num_epochs 200 --l1_penalty $l1"
