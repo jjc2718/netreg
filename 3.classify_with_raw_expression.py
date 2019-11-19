fix scaling bug with raw expression data

3.classify_with_raw_expression.py
@@ -41,7 +41,8 @@ np.random.seed(args.seed)
 algorithm = "raw"
 
 genes_df, pancan_data = du.load_raw_data(args.gene_list, verbose=args.verbose)
-rnaseq_train_df, rnaseq_test_df = du.load_expression_data(verbose=args.verbose)
+rnaseq_train_df, rnaseq_test_df = du.load_expression_data(verbose=args.verbose,
+                                                          scale_input=True)
 
 # Track total metrics for each gene in one file
 metric_cols = [